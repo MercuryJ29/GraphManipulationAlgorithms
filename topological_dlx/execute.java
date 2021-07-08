0012ffb3823fd5f85fc524233d6ee0ed32070e8b
MercuryJ29 <48653806+MercuryJ29@users.noreply.github.com>
2021-07-08 17:09:54 +0800

    Create execute.java

---
+++ topological_dlx/execute.java
@@ -0,0 +1,12 @@
+package topological_dlx;
+
+/*
+ * @author:Jiangzh
+ * 执行拓扑排序
+ * */
+public class execute {
+	public static void main(String[] args) {
+		sort topologicalSorting=new sort();
+		topologicalSorting.execute();
+	}
+}
