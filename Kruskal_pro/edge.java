6cd7fb904f28b309c773d52eec1ac7f561f0455a
MercuryJ29 <48653806+MercuryJ29@users.noreply.github.com>
2021-07-08 17:11:43 +0800

    Create edge.java

---
+++ Kruskal_pro/edge.java
@@ -0,0 +1,15 @@
+package kruskal_pro;
+
+
+/*
+ * @author:Jiangzh
+ * 边类
+ * 
+ * */
+public class edge {
+	public int endpoint1;			//边端点1
+	public int endpoint2;			//边端点2
+	public int value;				//该边的权值
+	public int equalEdgeCount=1;		//连续相等的边的数目
+	
+}
